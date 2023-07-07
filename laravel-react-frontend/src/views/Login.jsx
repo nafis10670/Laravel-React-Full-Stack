28e2d21950d4b3e0457636c2dc95d134ede070d6
Nafis Saami Azad <nafissazad@gmail.com>
2023-07-08 00:05:35 +0600

    Finish login and signup pages

--- laravel-react-frontend/src/views/Login.jsx
+++ laravel-react-frontend/src/views/Login.jsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
+// import axiosClient from "../axios-client.js";
+import { createRef } from "react";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+import { useState } from "react";
+
 export default function Login() {
-    return <div>Login</div>;
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Login into your account</h1>
+
+                    {message && (
+                        <div className="alert">
+                            <p>{message}</p>
+                        </div>
+                    )}
+
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Not registered?{" "}
+                        <Link to="/signup">Create an account</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
