94c8f4b09a58b72f631e9ebd8e1d0f34eee612b3
Nafis Saami Azad <nafissazad@gmail.com>
2023-07-09 22:21:55 +0600

    notifications added

--- laravel-react-frontend/src/contexts/ContextProvider.jsx
+++ laravel-react-frontend/src/contexts/ContextProvider.jsx
@@ -3,13 +3,16 @@ import { createContext, useContext, useState } from "react";
 const StateContext = createContext({
     currentUser: null,
     token: null,
+    notification: null,
     setUser: () => {},
     setToken: () => {},
+    setNotification: () => {},
 });
 
 export const ContextProvider = ({ children }) => {
     const [user, setUser] = useState({});
     const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+    const [notification, _setNotification] = useState("");
 
     const setToken = (token) => {
         _setToken(token);
@@ -20,6 +23,14 @@ export const ContextProvider = ({ children }) => {
         }
     };
 
+    const setNotification = (message) => {
+        _setNotification(message);
+
+        setTimeout(() => {
+            _setNotification("");
+        }, 5000);
+    };
+
     return (
         <StateContext.Provider
             value={{
@@ -27,6 +38,8 @@ export const ContextProvider = ({ children }) => {
                 setUser,
                 token,
                 setToken,
+                notification,
+                setNotification,
             }}
         >
             {children}
